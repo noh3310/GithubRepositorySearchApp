4529cdd923c006105046c34344a5eef8f8958f83
GunHo <noh3310@gmail.com>
2022-06-15 23:54:40 +0900

    #5 fix: HomeViewModel 수정

--- Kurly-Assignment/ViewModel/HomeViewModel.swift
+++ Kurly-Assignment/ViewModel/HomeViewModel.swift
@@ -20,15 +20,21 @@ final class HomeViewModel {
     
     var searchChar = BehaviorRelay<String>(value: "")
     var pageNumber = 1
+    var tableViewValue = BehaviorRelay<[RepoSearchResultItem]>(value: [])
     var repoData = BehaviorRelay<Repositorys>(value: Repositorys(incompleteResults: nil, items: nil, totalCount: nil))
     
     var disposeBag = DisposeBag()
     
+    func addNumber() {
+        pageNumber += 1
+        fetchRepo()
+    }
+    
     private func fetchRepo() {
         apiManager.searchRepo(apiManager.parameters(searchChar.value, pageNumber))
-            .subscribe(onNext: { repo in
+            .subscribe(onNext: { [weak self] repo in
                 if let repo = repo {
-                    self.repoData.accept(repo)
+                    self?.repoData.accept(repo)
                 }
             })
             .disposed(by: disposeBag)
@@ -40,10 +46,23 @@ extension HomeViewModel: RxBind {
     func bind() {
         searchChar
             .filter { $0.count > 0 }
-            .subscribe { _ in
-            self.fetchRepo()
+            .subscribe { [weak self] _ in
+                self?.pageNumber = 1
+                self?.fetchRepo()
         }
         .disposed(by: disposeBag)
+        
+        repoData
+            .subscribe(onNext: { [weak self] repositoryData in
+                if let items = repositoryData.items {
+                    if self?.pageNumber == 1 {
+                        self?.tableViewValue.accept(items)
+                    } else {
+                        let value = self?.tableViewValue.value
+                        self?.tableViewValue.accept((value ?? []) + items)
+                    }
+                }
+            }).disposed(by: disposeBag)
     }
     
 }
