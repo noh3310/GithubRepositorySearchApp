075bd301e40ba3cf1d010d3ed93f3703f0106b1b
GunHo <noh3310@gmail.com>
2022-06-16 07:46:24 +0900

    #8 feat: Reachabilith 구현

--- Kurly-Assignment/View/Detail/DetailViewController.swift
+++ Kurly-Assignment/View/Detail/DetailViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 import WebKit
 import SnapKit
+import Reachability
 
 final class DetailViewController: BaseViewController {
     
     let webView = WKWebView()
     var currentUrl: URL? = nil
+    
+    let reachability = try! Reachability()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +23,31 @@ final class DetailViewController: BaseViewController {
         addViews()
         setLayout()
         
-        let request = URLRequest(url: currentUrl!)
-        webView.load(request)
+        setReachability()
     }
     
+}
+
+extension DetailViewController {
+    func setReachability() {
+        reachability.whenReachable = { [weak self] reachability in
+            let request = URLRequest(url: (self?.currentUrl!)!)
+            self?.webView.load(request)
+        }
+        reachability.whenUnreachable = { [weak self] _ in
+            print("Not reachable")
+            let sheet = UIAlertController(title: "네트워크 통신을 수행할 수 없습니다.", message: "확인버튼을 클릭해주세요", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "확인", style: .default)
+            sheet.addAction(ok)
+            self?.present(sheet, animated: true)
+        }
+        
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+    }
 }
 
 extension DetailViewController: SetViews {
